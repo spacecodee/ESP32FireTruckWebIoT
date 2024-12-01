410015d6310d1bb8cd382f06ae9737067e187e70
spacecodee <spacecodee@gmail.com>
2024-12-01 15:17:18 -0500

    Implement responsive sidebar with toggle functionality and adjust layout styles

--- src/app/view/layouts/main-layout/main-layout.component.ts
+++ src/app/view/layouts/main-layout/main-layout.component.ts
@@ -1,11 +1,9 @@
-// view/pages/dashboard/main-layout.component.ts
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ThemeToggleComponent } from '@app/shared/components/theme-toggle/theme-toggle.component';
 import { LogoutButtonComponent } from '@app/shared/components/logout-button/logout-button.component';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from '@shared/components/sidebar/sidebar.component';
-import { WebSocketService } from '@core/services/websocket/websocket.service';
 import { ConnectionStatusComponent } from '@shared/components/connection-status/connection-status.component';
 
 @Component({
@@ -22,5 +20,11 @@ import { ConnectionStatusComponent } from '@shared/components/connection-status/
   templateUrl: './main-layout.component.html',
 })
 export class MainLayoutComponent {
-  constructor(public webSocketService: WebSocketService) {}
+  sidebarOpen = false;
+
+  constructor() {}
+
+  toggleSidebar() {
+    this.sidebarOpen = !this.sidebarOpen;
+  }
 }
